998458fe18755723c2201cde52a367d781fe8694
Julien Rousseau <julienrousseau16@gmail.com>
2020-04-01 16:19:14 +0200

    quest React 3 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
+import ContactList from './components/ContactList'
 
-import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Contact
-        name="Debra Howard"
-        avatar="https://randomuser.me/api/portraits/women/4.jpg"
-        online
-      />
-      <Contact
-        name="Francisco Gonzalez"
-        avatar="https://randomuser.me/api/portraits/men/43.jpg"
-        offline
-      />
-      <Contact
-        name="Sophie Perez"
-        avatar="https://randomuser.me/api/portraits/women/18.jpg"
-        online
-      />
+      <ContactList />
     </div>
   );
 }
